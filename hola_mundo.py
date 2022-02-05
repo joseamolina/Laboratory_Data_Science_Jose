4c7bc5d5b557e517a4d1a13142c9ea6a422ad9b2
joseamolina <iosifmh@gmail.com>
2022-02-05 17:02:09 +0100

    Primera version

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,1 +1,12 @@
 print("Hello world to everyone!")
+
+def search_target(arr, target):
+    conj = {}
+    for ix, i in enumerate(arr):
+        resta = target - i
+        if resta in conj:
+            return conj[resta], ix
+        conj[i] = ix
+    return ()
+
+print(search_target([1, 6], 5))
